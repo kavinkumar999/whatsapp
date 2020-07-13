9375000f64a0133a0a2023674b0d20c6d729d125
kavinkumar999 <kavinkumarnkm007@gmail.com>
2020-07-13 19:36:40 +0530

    ever

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def get_dictionary_response(word):
 
 @app.route("/")
 def home():
-    return "<h1>Hello whatsapp2 bot</h1>"
+    return "<h1>Hello ewhatsapp2 bot</h1>"
 
 
 if __name__ == "__main__":
